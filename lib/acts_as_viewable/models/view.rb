ac4379996b82e8d8eda40f5ea6ed09e673430efc
Olivier Berho <olivier.berho@gmail.com>
2012-02-05 18:31:56 +0100

    update ruby and rails

--- lib/acts_as_viewable/models/view.rb
+++ lib/acts_as_viewable/models/view.rb
@@ -8,20 +8,19 @@ module ActsAsViewable
     private
 
       def ip_has_not_viewed_viewable_in_last_n_minutes
-        time_ago = (viewable_type.camelcase.constantize::VIEW_TIME_TO_LIVE || 0).minutes.ago
         # views = View.all
-        # views.each do |v|
-        #   puts "MAX DELAY #{time_ago}"
-        #   puts "NOW #{Time.now.utc}"
-        #   puts "VU PAR : #{v.ip} | #{v.viewable_id} | #{v.viewable_type} A #{v.created_at}"
-        #   puts "#{v.created_at === (time_ago..Time.now)}"
-        #   puts "LE MEME" if (v.ip==ip && v.viewable_type == viewable_type && v.viewable_id == viewable_id && v.created_at === (time_ago...Time.now.utc))
-        # end
+         # views.each do |v|
+         #   puts "MAX DELAY #{time_ago}"
+         #   puts "NOW #{Time.now.utc}"
+         #   puts "VU PAR : #{v.ip} | #{v.viewable_id} | #{v.viewable_type} A #{v.created_at}"
+         #   puts "#{v.created_at === (time_ago..Time.now)}"
+         #   puts "LE MEME" if (v.ip==ip && v.viewable_type == viewable_type && v.viewable_id == viewable_id && v.created_at === (time_ago...Time.now.utc))
+         # end
         errors.add(:ip, 'has viewed this viewable too recently') unless View.count(:conditions => {
           :ip => ip,
           :viewable_id => viewable_id,
           :viewable_type => viewable_type,
-          :created_at => time_ago..Time.now.utc
+          :created_at => (viewable_type.camelcase.constantize::VIEW_TIME_TO_LIVE || 0).minutes.ago..Time.now
         }).zero?
       end
   end
